f74c8827ed96e65a265a944fcb9dcf50809c2226
Ddiog <alexis.vandenbogaert@oniryx.be>
2021-12-16 10:16:45 +0100

    init module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {MenuComponent} from "./menu/components/menu/menu.component";
+
+const routes: Routes =  [
+  {path: '', redirectTo:'menu', pathMatch: 'full'},
+  {path: 'menu', component: MenuComponent }
+  ]
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
