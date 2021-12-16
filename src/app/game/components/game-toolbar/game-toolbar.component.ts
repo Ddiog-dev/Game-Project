604a8dc116c7dcaf7436afde2e5d56a1d77ae534
Ddiog <alexis.vandenbogaert@oniryx.be>
2021-12-16 11:36:15 +0100

    game component and game toolbar

---
+++ src/app/game/components/game-toolbar/game-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-toolbar',
+  templateUrl: './game-toolbar.component.html',
+  styleUrls: ['./game-toolbar.component.css']
+})
+export class GameToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
