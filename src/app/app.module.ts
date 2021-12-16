c42b3afc1b71edb4c70ac27e00f71d737f8801ab
Ddiog <alexis.vandenbogaert@oniryx.be>
2021-12-16 11:35:27 +0100

    create menu and game page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MenuComponent } from './menu/components/menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {GameModule} from "./game/game.module";
 
 @NgModule({
   declarations: [
@@ -13,8 +17,14 @@ import { MenuComponent } from './menu/components/menu/menu.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    GameModule
   ],
   providers: [],
+  exports: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GameComponent } from './game.component';
+import {GameToolbarComponent} from "./components/game-toolbar/game-toolbar.component";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GameComponent,
+    GameToolbarComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  exports: [
+    GameToolbarComponent
+  ],
 })
 export class GameModule { }
